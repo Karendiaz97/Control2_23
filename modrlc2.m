aefe2a01c370951e0272bbff6b4a0dac6874a281
Karendiaz97 <karenluciladiaz@gmail.com>
2023-05-09 18:31:25 -0300

    ejer1

--- modrlc2.m
+++ modrlc2.m
@@ -4,10 +4,10 @@ L=10e-6;
 C=100e-9;
 A=[-R/L -1/L ; 1/C 0];
 B= [1/L ; 0];
-h=t_etapa/10; %Se lo divide por 10 y se lo itera 10 veces en el for
+h=9e-12; %TR tiempo rapido
 u=accion;
 x= xant;
-for ii=1:10
+for ii=1:t_etapa/h
  xp= A*x+B*u;
  x=x+xp*h;
 end
